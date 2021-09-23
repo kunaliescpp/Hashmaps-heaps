6aa66689506008c2b6970fd3a26d87873cba98f6
kunal_cpp <kunaliesc@gmail.com>
2021-09-23 17:52:37 +0530

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,1 +1,85 @@
+/*
+Problem Link: https://leetcode.com/problems/sliding-window-maximum/
+
+Sliding Window Maximum
+
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+Example 1:
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+  
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+
+Example 3:
+Input: nums = [1,-1], k = 1
+Output: [1,-1]
+
+Example 4:
+Input: nums = [9,11], k = 2
+Output: [11]
+
+Example 5:
+Input: nums = [4,-2], k = 2
+Output: [4]
+ 
+
+Constraints:
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
+1 <= k <= nums.length
+*/
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        
+        int n = arr.size(); 
+        // Next greater of every element
+             vector<int>v;
+             stack<int>stk;
+        
+             stk.push(n-1);
+             v.push_back(n);
+        
+           for(int i = n-2; i >= 0; i--){
+ 
+               while(!stk.empty() && arr[i] >= arr[stk.top()]) stk.pop();
+            
+                  int nextGreater = (stk.empty()) ? (n) : (stk.top());
+
+                  v.push_back(nextGreater);
+                  stk.push(i);
+            }
+        
+        reverse(v.begin(), v.end());
+        
+        // Slide window
+        vector<int> Vec;
+        int j = 0;
+        for(int i = 0; i <= n-k; i++){
+            if(j < i) j = i;
+            
+            while(v[j] <= i+k-1) j = v[j];
+            
+           Vec.push_back(arr[j]); 
+        }
+        
+    return Vec;  
+    }
+};
 
