30ef8c1b78c0b020329f3ad94ec7dda3d3662861
kunal_cpp <kunaliesc@gmail.com>
2021-10-18 13:51:49 +0530

    Update Check AP.cpp

--- Check AP.cpp
+++ Check AP.cpp
@@ -1,1 +1,43 @@
+/* Problem Link: https://practice.geeksforgeeks.org/problems/check-arithmetic-progression1842/1
 
+Check Arithmetic Progression 
+
+Given an array of N integers. Write a program to check whether an arithmetic progression can be formed using all the given elements. 
+ 
+Example 1:
+Input: N=4
+arr[] = { 0,12,4,8 }
+Output: YES
+Explanation: Rearrange given array as {0, 4, 8, 12}  which forms an arithmetic progression.
+
+Example 2:
+Input: N=4
+arr[] = {12, 40, 11, 20}
+Output: NO
+
+Constraints:
+2 ≤ N ≤ 10^5
+*/
+  
+
+bool checkIsAP(int arr[], int n){
+
+    unordered_map<int, bool> mp;
+    if(n <= 1) return true;
+
+    int mini = 1e9, maxi = -1e9;
+    for(int i = 0; i < n; i++){
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+        mp[arr[i]] = true;
+    }
+
+    int d = (maxi - mini) / (n-1);
+    for(int i = 0; i < n; i++){
+        int ai = mini + i * d;
+        if(mp.count(ai) == 0) return false;
+    }
+
+return true;
+}
+ 
