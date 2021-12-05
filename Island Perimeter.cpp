c1dc43fa4afc7f58d67fa672832728e389921412
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 11:10:12 +0530

    Update Island Perimeter.cpp

--- Island Perimeter.cpp
+++ Island Perimeter.cpp
@@ -5,11 +5,11 @@ Island Perimeter
 
 You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
 
-Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island 
-(i.e., one or more connected land cells).
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is 
+exactly one island (i.e., one or more connected land cells).
 
-The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1.
-The grid is rectangular,width and height don't exceed 100. Determine the perimeter of the island.
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square
+with side length 1. The grid is rectangular,width and height don't exceed 100. Determine the perimeter of the island.
 
 Example 1:
 Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
@@ -24,9 +24,8 @@ Example 3:
 Input: grid = [[1,0]]
 Output: 4
  
-
+ 
 Constraints:
-
 row == grid.length
 col == grid[i].length
 1 <= row, col <= 100
@@ -62,4 +61,6 @@ public:
     return perimeter;
     }
 };
+
+
 
