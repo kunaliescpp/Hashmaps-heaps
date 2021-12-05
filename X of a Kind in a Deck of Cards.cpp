e6d41028b8fea96aceb6d59075f557a1cc478f70
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 11:39:40 +0530

    Update X of a Kind in a Deck of Cards.cpp

--- X of a Kind in a Deck of Cards.cpp
+++ X of a Kind in a Deck of Cards.cpp
@@ -4,13 +4,10 @@ Problem Link: https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
 X of a Kind in a Deck of Cards
 
 In a deck of cards, each card has an integer written on it.
-
 Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
-
 Each group has exactly X cards.
 All the cards in each group have the same integer.
  
-
 Example 1:
 Input: deck = [1,2,3,4,4,3,2,1]
 Output: true
@@ -56,3 +53,5 @@ public:
     return g > 1;                                     // gcd of 7 and 2 is 1
     }
 };
+
+
