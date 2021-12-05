48a6530f09fab4c35909fb28923746be61fa3efd
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 12:37:22 +0530

    Update Binary heap operations.cpp

--- Binary heap operations.cpp
+++ Binary heap operations.cpp
@@ -12,24 +12,17 @@ in Binary Heap. The same property must be recursively true for all nodes in Bina
 
 You are given an empty Binary Min Heap and some queries and your task is to implement the three methods insertKey, deleteKey, and 
 extractMin on the Binary Min Heap and call them as per the query given below:
-1) 1  x  (a query of this type means to insert an element in the min-heap with value x )
-2) 2  x  (a query of this type means to remove an element at position x from the min-heap)
-3) 3  (a query like this removes the min element from the min-heap and prints it ).
+1) insertKey(x): A query of this type means to insert an element in the min-heap with value x )
+2) deleteKey(x): A query of this type means to remove an element at position x from the min-heap)
+3) extractMin(): A query like this removes the min element from the min-heap and prints it ).
 
 Example 1:
-
-Input: Q = 7
-Queries:
-insertKey(4)
-insertKey(2)
-extractMin()
-insertKey(6)
-deleteKey(0)
-extractMin()
-extractMin()
+Input: 
+Q = 7
+Queries: insertKey(4), insertKey(2), extractMin(), insertKey(6), deleteKey(0), extractMin(), extractMin()
 Output: 2 6 - 1
 Explanation: In the first test case for query 
-insertKey(4) the heap will have  {4}  
+insertKey(4) the heap will have {4}  
 insertKey(2) the heap will be {2 4}
 extractMin() removes min element from heap ie 2 and prints it now heap is {4} 
 insertKey(6) inserts 6 to heap now heap is {4 6}
@@ -37,13 +30,10 @@ deleteKey(0) delete element at position 0 of the heap,now heap is {6}
 extractMin() remove min element from heap ie 6 and prints it now the heap is empty
 extractMin() since the heap is empty thus no min element exist so -1 is printed.
 
-Example 2:Input: Q = 5
-Queries:
-insertKey(8)
-insertKey(9)
-deleteKey(1)
-extractMin()
-extractMin()
+Example 2:
+Input: 
+Q = 5
+Queries: insertKey(8), insertKey(9), deleteKey(1), extractMin(), extractMin()
 Output: 8 -1
 
 Constraints:
