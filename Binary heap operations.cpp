f55798b26d65f4b18459387dd2a9f39e096a5e84
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 12:38:52 +0530

    Update Binary heap operations.cpp

--- Binary heap operations.cpp
+++ Binary heap operations.cpp
@@ -12,9 +12,9 @@ in Binary Heap. The same property must be recursively true for all nodes in Bina
 
 You are given an empty Binary Min Heap and some queries and your task is to implement the three methods insertKey, deleteKey, and 
 extractMin on the Binary Min Heap and call them as per the query given below:
-1) insertKey(x): A query of this type means to insert an element in the min-heap with value x )
-2) deleteKey(x): A query of this type means to remove an element at position x from the min-heap)
-3) extractMin(): A query like this removes the min element from the min-heap and prints it ).
+1) insertKey(x): A query of this type means to insert an element in the min-heap with value x.
+2) deleteKey(x): A query of this type means to remove an element at position x from the min-heap.
+3) extractMin(): A query like this removes the min element from the min-heap and prints it .
 
 Example 1:
 Input: 
