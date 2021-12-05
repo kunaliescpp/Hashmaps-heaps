a6043030f6baad3971dbe26b4ab4069ed7e61beb
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 11:23:26 +0530

    Update Longest Consecutive Sequence.cpp

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -5,8 +5,6 @@ Longest Consecutive Sequence
 
 Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
-You must write an algorithm that runs in O(n) time.
-
 Example 1:
 Input: nums = [100,4,200,1,3,2]
 Output: 4
@@ -18,7 +16,6 @@ Output: 9
  
 
 Constraints:
-
 0 <= nums.length <= 10^5
 -10^9 <= nums[i] <= 10^9
 */
@@ -46,3 +43,5 @@ public:
     return len;
     }
 };
+
+
