9d0ba3f9e64b13a0cf3b2be6f69dc5b46bc4eff0
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 11:22:48 +0530

    Update Line reflection.cpp

--- Line reflection.cpp
+++ Line reflection.cpp
@@ -1,35 +1,38 @@
-/* problem Link: https://www.lintcode.com/problem/908/
+/* 
+problem Link: https://www.lintcode.com/problem/908/
 
 Line Reflection
 
 Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
 
-Example1:
+Example 1:
 Input: [[1,1],[-1,1]]
 Output: true
 
-Example2:
+Example 2:
 Input: [[1,1],[-1,-1]]
 Output: false
 */
     
-  bool isReflected(vector<vector<int>> &points) {
+bool isReflected(vector<vector<int>> &points) {
 
-      int n = points.size();
-      int maxi = INT_MIN, mini = INT_MAX;
-      unordered_map<int, set<int> > mp;
+  int n = points.size();
+  int maxi = INT_MIN, mini = INT_MAX;
+  unordered_map<int, set<int> > mp;
 
-      for(int i = 0; i < n; i++){
-          mini = min(mini, points[i][0]);
-          maxi = max(maxi, points[i][0]);
-          mp[points[i][0]].insert(points[i][1]); 
-      }
-
-      double y = double (maxi + mini)/2.0;
-      for(int i = 0; i < n; i++){
-          int x2 = 2*y-points[i][0];   //x2 = 2*y-x1
-          // int y2 = y1;
-          if(mp.count(x2) == false || mp[x2].count(points[i][1]) == false) return false;
-      }
-  return true;
+  for(int i = 0; i < n; i++){
+      mini = min(mini, points[i][0]);
+      maxi = max(maxi, points[i][0]);
+      mp[points[i][0]].insert(points[i][1]); 
   }
+
+  double y = double (maxi + mini)/2.0;
+  for(int i = 0; i < n; i++){
+      int x2 = 2*y-points[i][0];   //x2 = 2*y-x1
+      // int y2 = y1;
+      if(mp.count(x2) == false || mp[x2].count(points[i][1]) == false) return false;
+  }
+return true;
+}
+
+
