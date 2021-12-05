9c56dcb202ffad020c766f88895f77413258aa4b
kunal_cpp <kunaliesc@gmail.com>
2021-12-05 11:37:18 +0530

    Update Length of largest subarray with contiguous elements 1.cpp

--- Length of largest subarray with contiguous elements 1.cpp
+++ Length of largest subarray with contiguous elements 1.cpp
@@ -1,9 +1,11 @@
 /*
 Problem Link: https://www.geeksforgeeks.org/length-largest-subarray-contiguous-elements-set-1/
 
-Given an array of distinct integers, find length of the longest subarray which contains numbers that can be arranged in a continuous sequence. 
-Examples: 
+Length of largest subarray with contiguous elements 
 
+Given an array of distinct integers, find length of the longest subarray which contains numbers that can be arranged in a continuous sequence.
+
+Examples: 
 Input:  arr[] = {10, 12, 11};
 Output: Length of the longest contiguous subarray is 3
 
@@ -12,3 +14,29 @@ Output: Length of the longest contiguous subarray is 2
 
 Input:  arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
 Output: Length of the longest contiguous subarray is 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
