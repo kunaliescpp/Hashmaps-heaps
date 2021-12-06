dd37763177b2bec36669642e0a5b12a001a27143
kunal_cpp <kunaliesc@gmail.com>
2021-12-06 19:31:47 +0530

    Update Subarray Sums Divisible by K.cpp

--- Subarray Sums Divisible by K.cpp
+++ Subarray Sums Divisible by K.cpp
@@ -27,28 +27,23 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         
-        int cnt = 0;
+        int n = nums.size();
        
-        unordered_map<int, int> um; 
+        unordered_map<int, int> mp; 
         int sum = 0;
         int rem = 0;
         
         um[0] = 1;                                                              
-        
-        for(int i = 0; i < nums.size(); i++){                                  
+        int cnt = 0;
+        for(int i = 0; i < n; i++){                                  
            sum = sum + nums[i];
+           
            rem = sum % k;
-           if(rem < 0){
-               rem+=k;
-           }
-        
-           if(um.find(rem) != um.end()){
-               cnt += um[rem];
-               um[rem]++;
-           }
-        
-          else  um[rem]++;
+           if(rem < 0) rem+=k;
+           
+           if(mp.count(rem) == 1) cnt += um[rem];
           
+           um[rem]++;
         }
      return cnt;
     }
