e82b149cb94ee61209221e95281d4f3ca1bf4a20
kunal_cpp <kunaliesc@gmail.com>
2022-02-05 20:50:28 +0530

    Update Group Anagrams.cpp

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -4,36 +4,25 @@ Problem Link: https://leetcode.com/problems/group-anagrams/
 Group Anagrams
 
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original 
+letters exactly once.
 
 Example 1:
 Input: strs = ["eat","tea","tan","ate","nat","bat"]
 Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-Example 2:
-Input: strs = [""]
-Output: [[""]]
-
-Example 3:
-Input: strs = ["a"]
-Output: [["a"]]
- 
-
 Constraints:
-
 1 <= strs.length <= 104
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 */
-  
   
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
        
         map< map<char,int> , vector<string>>bmp;
-        
+     
         for( auto i: strs){
             map<char,int>mp;
             for(auto j: i){
@@ -49,3 +38,5 @@ public:
     return v;
     }
 };
+
+
