da38e1bdaeca8e12737e8289281e6af12d9199f7
kunal_cpp <kunaliesc@gmail.com>
2022-02-09 19:51:50 +0530

    Update Top K frequent elements.cpp

--- Top K frequent elements.cpp
+++ Top K frequent elements.cpp
@@ -22,36 +22,21 @@ It is guaranteed that the answer is unique.
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 */
 
-class Solution {
-public:
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int>v;
+    unordered_map<int,int>mp;
+    for(int i: nums) mp[i]++;                                                                      //O(n)
 
-  struct cmp{   // we use struct compare in case of priority queue
+    priority_queue<pair<int, int>> pq; 
+    for(auto &x : mp){
+        pq.push({x.second, x.first});
+    }
 
-      bool operator()(pair<int, int>& a, pair<int, int>& b){
-          if(a.second == b.second) return a.first > b.first;      // a.first < b.first
-                                                                  // (max heap = reverse of sorting)
-          if(a.second < b.second) return true;
+    for(int i = 0; i < k; i++){                                                              //O(k * log n)
+        v.push_back(pq.top().second);
+        pq.pop();
+    }
 
-      return false;
-      }
-  };
-
-  vector<int> topKFrequent(vector<int>& nums, int k) {
-
-      vector<int>v;
-      unordered_map<int,int>mp;
-
-      for(int i: nums) mp[i]++;                                                                      //O(n)
-
-      priority_queue< pair<int,int>, vector<pair<int,int>>, cmp> pq(mp.begin(), mp.end());           //O(n)
-
-      for(int i = 0; i < k; i++){                                                              //O(k * log n)
-          v.push_back(pq.top().first);
-          pq.pop();
-      }
-
-  return v;
-  }
-};
-
+return v;
+}
 
