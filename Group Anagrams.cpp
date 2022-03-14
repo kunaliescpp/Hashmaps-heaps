54924f3bd56675c3f5743674137f648886074fe0
kunal_cpp <kunaliesc@gmail.com>
2022-03-14 16:24:04 +0530

    Update Group Anagrams.cpp

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -16,27 +16,25 @@ Constraints:
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 */
-  
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       
-        map< map<char,int> , vector<string>>bmp;
-     
-        for( auto i: strs){
-            map<char,int>mp;
-            for(auto j: i){
-                mp[j]++;
-            }
-           bmp[mp].push_back(i);
-        }
-    
-        vector<vector<string>>v;
-        for(auto x: bmp){
-            v.push_back(x.second);
-        }
-    return v;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+   int n = strs.size();
+
+    map<vector<int>, vector<string>> mp;
+    for(int i =  0; i < n; i++){
+        // checking the freq of curr string
+        string str = strs[i];
+        vector<int>freq(26, 0);
+        for(char c: str) freq[c-'a']++;
+
+        mp[freq].push_back(str);
     }
-};
+
+    vector<vector<string>> ans;
+    for(auto x: mp){
+        ans.push_back(x.second);
+    }
+return ans;
+}
 
 
